86df21d164bda2e865211c3a1a99016a13515ae3
admin <admin@YD2-01>
2022-04-11 14:23:21 +0900

    조건문2

---
+++ HelloWorld/src/co/edu/variable/IfExample2.java
@@ -0,0 +1,17 @@
+package co.edu.variable;
+
+public class IfExample2 {
+	public static void main(String[] args) {
+		int num1, num2;
+		
+		num1 = 135;
+		num2 = 353;
+		
+		if(num1 > num2) {
+			System.out.println("큰수는 : " + num1);
+		}else {
+			System.out.println("큰수는 : " + num2);
+		}
+	}
+
+}
